7394f7524dcd7c088bf89296fcab783a62226f29
elwafa <eslamaboelwafa@gmail.com>
2024-12-07 14:40:46 +0200

    tag-manager

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import TagManager from 'react-gtm-module';
 import './index.css';
 import App from './App';
 
+// Initialize Google Tag Manager
+const tagManagerArgs = {
+    gtmId: 'GTM-NCCBX5MK', // Replace with your GTM ID
+};
+TagManager.initialize(tagManagerArgs);
+
+// Render the React application
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
